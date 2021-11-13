3417c486528decdef462ac28fc701ddf4c290e5c
zorcat27 <bharwood@pdx.edu>
2021-11-12 22:23:32 -0800

    cleaned up header

--- parser/parser.h
+++ parser/parser.h
@@ -23,12 +23,8 @@ typedef struct _parser_s
 	parser_state_t		lineState;
 } parser_t, *parserPtr_t;
 
-parser_state_t getLine(parserPtr_t parser, inputCommandPtr_t newCommand, unsigned long long currentTime);
-
 // Function Definitions
-bool debugMode();
-FILE *openFile();
-const char* getCommand(operation_t command);
-bool readLine(FILE *inputFile, inputCommandPtr_t currentLine);
+parser_state_t getLine(parserPtr_t parser, inputCommandPtr_t newCommand, unsigned long long currentTime);
+parserPtr_t initParser(char* inputFile);
 
 #endif
